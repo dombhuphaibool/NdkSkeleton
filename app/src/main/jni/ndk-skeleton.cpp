222633d6a83b1f6371b10a7b8d9759bdd653d61d
Dom Bhuphaibool <dombhuphaibool@gmail.com>
2015-11-14 21:48:08 -0800

    Fix exporting of entry point

--- app/src/main/jni/ndk-skeleton.cpp
+++ app/src/main/jni/ndk-skeleton.cpp
@@ -1,8 +1,16 @@
 #include <jni.h>
 #include <string>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 JNIEXPORT jstring JNICALL
 Java_com_bandonleon_ndkskeleton_MainActivity_getOutputString(JNIEnv *env, jobject instance) {
     std::string returnValue = "Hello from native land!";
     return env->NewStringUTF(returnValue.c_str());
 }
+
+#ifdef __cplusplus
+}
+#endif
